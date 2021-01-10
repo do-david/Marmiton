51f9dc5b1bceb39f892a385fc703fd8fee995e5e
do-david <50655142+do-david@users.noreply.github.com>
2021-01-11 00:18:08 +0100

    add title below image

--- marmiton/src/components/home/index.js
+++ marmiton/src/components/home/index.js
@@ -9,17 +9,26 @@ const HomeComponent = () => {
     const [error, setError] = useState('')
     const [recipes, setRecipes] = useState([])
     useEffect(()=>{
-        getRecipes('burger',setIsLoaded,setRecipes,setError)
+        getRecipes('kebab',setIsLoaded,setRecipes,setError)
     },[])
     console.log(recipes)
     return(
         <RowContainer>
             <ColumnContainer>
             {recipes.map((recipe,index) => {
+                const url = `${BaseUri}${recipe.image}`
+                console.log(url)
                 return(
-                    //<img href="`${BaseUri}${recipe.image}`"></img>
-                    //<Image href="`${BaseUri}`${recipe.image}"></Image>
-                    <span>{recipe.title}</span>
+                    <>
+                    <Image src={url}></Image>
+                    <TextBlock>
+                        <TextContainer>
+                            <TitleBlock>
+                                <h2>{recipe.title}</h2>
+                            </TitleBlock>
+                        </TextContainer>
+                    </TextBlock>
+                    </>
                 )
             }
             )
@@ -33,13 +42,40 @@ const RowContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   padding: 0 15px;
+  height:auto;
+
 `
 const ColumnContainer = styled.div`
-  flex: 24%;
+  flex: 25%;
   padding: 0 15px;
+  height:auto;
+`
+const TextBlock = styled.div`
+padding-top: 10px;
+`
+const TextContainer = styled.div`
+position: relative;
+z-index: 50;
+margin-top: -15px;
+padding-bottom: 15px;
+border-radius: 15px;
+background-color: #ff6f61;
+overflow: hidden;
+box-shadow: 0 6px 9px 0 rgba(0, 0, 0, 0.15);
+`
+const TitleBlock = styled.div`
+padding: 5px 20px;
+font-family: 'Filson Soft',Arial,sans-serif;
+font-weight: bold;
+text-align: center;
+font-size: 20px;
+line-height: 1.2;
 `
 const Image = styled.img`
+border-radius: 30%;
 margin-top: 8px;
 vertical-align: middle;
+width:250px;
+height:350px;
 `
 export default HomeComponent
